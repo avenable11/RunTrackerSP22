4763be11b729294458f9accbf78e5fffae2f7bba
Angela Venable <avenable11@ivytech.edu>
2022-04-27 21:32:33 -0400

    Database

---
+++ app/src/main/java/edu/ivytech/runtrackersp22/database/LocationConverter.kt
@@ -0,0 +1,16 @@
+package edu.ivytech.runtrackersp22.database
+
+import androidx.room.TypeConverter
+import java.util.*
+
+class LocationConverter {
+    @TypeConverter
+    fun fromDate(date: Date): Long {
+        return date.time
+    }
+
+    @TypeConverter
+    fun toDate(millisSinceEpoch : Long?) : Date? {
+        return millisSinceEpoch?.let { Date(it) }
+    }
+}
---
+++ app/src/main/java/edu/ivytech/runtrackersp22/database/RunDatabase.kt
@@ -0,0 +1,12 @@
+package edu.ivytech.runtrackersp22.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+
+@Database(entities = [RunLocation::class], version = 1)
+@TypeConverters(LocationConverter::class)
+abstract class RunDatabase : RoomDatabase() {
+    abstract fun locationDao() : LocationDAO
+}
